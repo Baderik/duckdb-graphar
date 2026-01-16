6f4d649986a9c9768f196f067acd3bd3bdbf9abb
p.baderik <p.baderik@tbank.ru>
2026-01-16 10:49:21 +1000

    temp fix to string

--- include/readers/low_edge_reader.hpp
+++ include/readers/low_edge_reader.hpp
@@ -47,7 +47,7 @@ public:
         auto offset_in_chunk = offset.first % edge_info->GetChunkSize();
         auto count = offset.second - offset.first;
         Value path_list_val = Value::LIST(paths_val);
-        DUCKDB_GRAPHAR_LOG_TRACE("Reader::params: " + offset_in_chunk.to_string() + " " + count.to_string());
+        DUCKDB_GRAPHAR_LOG_TRACE("Reader::params: " + std::to_string(offset_in_chunk) + " " + std::to_string(count));
         result = std::move(conn->Query(query, path_list_val, offset_in_chunk, count));
     }
 
