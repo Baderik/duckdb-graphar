c9d07a25a8070cd817fb03becefd4c117465e557
p.baderik <p.baderik@tbank.ru>
2026-05-20 15:31:51 +1000

    feat(graphar_info): add function to get information of extension build

---
+++ include/functions/table/graphar_info.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+
+#include <duckdb/function/table_function.hpp>
+#include <duckdb.hpp>
+
+namespace duckdb {
+
+class GraphArVersionData : public GlobalTableFunctionState {
+public:
+    GraphArVersionData() : finished(false) {}
+
+    bool finished;
+};
+
+class GraphArInfo : public TableFunction {
+public:
+    static unique_ptr<FunctionData> Bind(ClientContext& context, TableFunctionBindInput& input,
+                                         vector<LogicalType>& return_types, vector<string>& names);
+    static unique_ptr<GlobalTableFunctionState> Init(ClientContext& context, TableFunctionInitInput& input);
+    static void Execute(ClientContext& context, TableFunctionInput& input, DataChunk& output);
+    static TableFunctionSet GetFunctions();
+    static void Register(ExtensionLoader& loader) { loader.RegisterFunction(GetFunctions()); }
+};
+
+}  // namespace duckdb
